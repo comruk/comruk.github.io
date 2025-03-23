648479eaadd202e5779c9e0b33785e35b853a26e
comruk <info@comruk.com>
2025-03-23 07:40:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
 function setLanguage(lang) {
-    alert("Language switch to: " + (lang === 'tr' ? "Türkçe" : "English"));
+  if (lang === 'tr') {
+    document.querySelector('h1').innerText = "COMRUK";
+    document.querySelector('.tagline').innerText = "Önce Dürüstlük, Her Zaman Güven";
+    document.querySelectorAll('p')[1].innerText = "Yakında yayındayız.";
+    document.querySelectorAll('p')[2].innerHTML = 'İletişim: <a href="mailto:info@comruk.com">info@comruk.com</a>';
+  } else {
+    location.reload();
+  }
 }
